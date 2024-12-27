6170b0f47a2b15ea07e448f7a657f170072659da
Amit Agarwal <>
2024-12-27 21:28:12 +0530

    Fix: Remove unused dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { Module } from '@nestjs/common';
 import { WzConfigModule } from './config/config.module';
 import { WzAccountsManagementModule } from './feature/accounts-management/accounts-management.module';
 import { AccountsController } from './feature/accounts-management/accounts.controller';
-import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [WzConfigModule, WzAccountsManagementModule, TransactionsModule],
+  imports: [WzConfigModule, WzAccountsManagementModule],
   controllers: [AccountsController],
   providers: [],
 })
